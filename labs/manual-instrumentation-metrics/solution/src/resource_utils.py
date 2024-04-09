4e2c85e590999ebb8eed30bb19a187bdc0619fa7
Jens Plüddemann <33718194+JenSeReal@users.noreply.github.com>
2024-04-09 14:36:38 +0200

    adds metrics and launch specification (#4)
    Co-authored-by: Jens Plüddemann <jens.plueddemann@novatec-gmbh.de>

---
+++ labs/manual-instrumentation-metrics/solution/src/resource_utils.py
@@ -0,0 +1,23 @@
+import socket
+
+from opentelemetry.sdk.resources import Resource, ResourceDetector
+from opentelemetry.semconv.resource import ResourceAttributes
+
+class HostDetector(ResourceDetector):
+    def detect(self) -> Resource:
+        return Resource.create(
+            {
+                ResourceAttributes.HOST_NAME: socket.gethostname(),
+            }
+        )
+
+def create_resource(name: str, version: str) -> Resource:
+    svc_rc = Resource.create(
+        {
+            ResourceAttributes.SERVICE_NAME: name,
+            ResourceAttributes.SERVICE_VERSION: version,
+        }
+    )
+    host_rc = HostDetector().detect()
+    rc = host_rc.merge(svc_rc)
+    return rc
