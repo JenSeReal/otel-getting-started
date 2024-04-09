2b55116b39e0c08293da99d12746980eeb93f001
Jens Plüddemann <33718194+JenSeReal@users.noreply.github.com>
2024-04-09 11:14:19 +0200

    adds the tracing lab (#3)
    Co-authored-by: Jens Plüddemann <jens.plueddemann@novatec-gmbh.de>

---
+++ labs/manual-instrumentation-traces/solution/src/trace_utils.py
@@ -0,0 +1,22 @@
+from opentelemetry import trace
+from opentelemetry.sdk.trace import TracerProvider
+from opentelemetry.sdk.trace.export import ConsoleSpanExporter, BatchSpanProcessor
+
+from resource_utils import create_resource
+
+
+def create_tracing_pipeline() -> BatchSpanProcessor:
+    console_exporter = ConsoleSpanExporter()
+    span_processor = BatchSpanProcessor(console_exporter)
+    return span_processor
+
+def create_tracer(name: str, version: str) -> trace.Tracer:
+    rc = create_resource(name, version)
+    processor = create_tracing_pipeline()
+    provider = TracerProvider(
+        resource=rc,
+        active_span_processor=processor
+    )
+    trace.set_tracer_provider(provider)
+    tracer = trace.get_tracer(name, version)
+    return tracer
