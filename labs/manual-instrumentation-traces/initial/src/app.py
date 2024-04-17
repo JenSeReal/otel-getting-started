5b24861cb66e7d79bf2d7efa70b8931920ef3478
Bob Smith <paulin.ledesert@wanadoo.fr>
2024-04-17 15:15:26 +0200

    Finished the trace manual lab

--- labs/manual-instrumentation-traces/initial/src/app.py
+++ labs/manual-instrumentation-traces/initial/src/app.py
@@ -5,15 +5,30 @@ import time
 import requests
 from client import ChaosClient, FakerClient
 from flask import Flask, make_response, request
+from opentelemetry import context
 from opentelemetry import trace as trace_api
 from opentelemetry.semconv.trace import SpanAttributes
 from trace_utils import create_tracer
+from opentelemetry.propagate import inject, extract
 
 # global variables
 app = Flask(__name__)
 tracer = create_tracer("app.py", "0.1")
 
+@app.before_request
+def before_request_func():
+    ctx = extract(request.headers)
+    previous_ctx = context.attach(ctx)
+    request.environ["previous_ctx_token"] = previous_ctx
+
+@app.teardown_request
+def teardown_request_func(err):
+    previous_ctx = request.environ.get("previous_ctx_token", None)
+    if previous_ctx:
+        context.detach(previous_ctx)
+
 @app.route("/users", methods=["GET"])
+@tracer.start_as_current_span("users")
 def get_user():
     user, status = db.get_user(123)
     data = {}
@@ -24,9 +39,12 @@ def get_user():
 
 @tracer.start_as_current_span("do_stuff")
 def do_stuff():
+    headers = {}
+    inject(headers)
+
     time.sleep(0.1)
     url = "http://httpbin:80/anything"
-    request = requests.get(url)
+    response = requests.get(url, headers=headers)
     print("response from httpbin:")
     print(response.text)
 
